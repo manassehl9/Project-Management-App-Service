08ce0b05f2b09503019d6c3ce46e04594db716aa
Manie <manassehl9@gmail.com>
2019-02-28 16:50:29 +0100

    Create Project Task endpoint

--- src/main/java/io/maieapp/ppmtool/controller/BacklogController.java
+++ src/main/java/io/maieapp/ppmtool/controller/BacklogController.java
@@ -1,5 +1,6 @@
 package io.maieapp.ppmtool.controller;
 
+import io.maieapp.ppmtool.domain.Project;
 import io.maieapp.ppmtool.domain.ProjectTask;
 import io.maieapp.ppmtool.services.MapValidationErrorService;
 import io.maieapp.ppmtool.services.ProjectTaskService;
@@ -37,5 +38,11 @@ public class BacklogController {
     @GetMapping("/{backlog_id}")
     public Iterable<ProjectTask> getProjectBacklog(@PathVariable String backlog_id){
         return projectTaskService.findBacklogById(backlog_id);
+    }
+
+    @GetMapping("/{backlog_id}/{pt_id}")
+    public ResponseEntity<?> getProjectTask(@PathVariable String backlog_id, @PathVariable String pt_id){
+        ProjectTask projectTask = projectTaskService.findPTByProjectSequence(backlog_id, pt_id);
+        return new ResponseEntity<ProjectTask>(projectTask, HttpStatus.OK);
     }
 }
--- src/main/java/io/maieapp/ppmtool/services/ProjectTaskService.java
+++ src/main/java/io/maieapp/ppmtool/services/ProjectTaskService.java
@@ -1,14 +1,15 @@
 package io.maieapp.ppmtool.services;
 
 import io.maieapp.ppmtool.domain.Backlog;
+import io.maieapp.ppmtool.domain.Project;
 import io.maieapp.ppmtool.domain.ProjectTask;
 import io.maieapp.ppmtool.exceptions.ProjectNotFoundException;
 import io.maieapp.ppmtool.repositories.BacklogRepository;
+import io.maieapp.ppmtool.repositories.ProjectRepository;
 import io.maieapp.ppmtool.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 public class ProjectTaskService {
@@ -18,6 +19,9 @@ public class ProjectTaskService {
 
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
+
+    @Autowired
+    private ProjectRepository projectRepository;
 
     public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask){
 
@@ -48,7 +52,18 @@ public class ProjectTaskService {
     }
 
     public Iterable<ProjectTask> findBacklogById(String id) {
+        Project project = projectRepository.findByProjectIdentifier(id);
+
+        if(project == null){
+            throw new ProjectNotFoundException("Project with ID:  '"+id+"' does not exist");
+        }
         return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
+    }
+
+    public ProjectTask findPTByProjectSequence(String backlog_id, String pt_id){
+        //make sure we are searching on the right backlog
+
+        return projectTaskRepository.findByProjectSequence(pt_id);
     }
 }
 
